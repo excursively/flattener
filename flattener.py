b41dc416a93829e330c998df6208b4796a352fec
Sataporn S <nothing2give@gmail.com>
2015-05-26 15:48:42 +0700

    add flattener class

---
+++ flattener.py
@@ -0,0 +1,23 @@
+class Flattener(object):
+
+    def flatten(self, data, prefix=""):
+        valuedict = dict()
+
+        if type(data) is dict:
+            for key in data:
+                dictdata = data[key]
+                if prefix == "":
+                    newprefix = key
+                else:
+                    newprefix = prefix + "__" + key
+
+                returndict = self.flatten(dictdata, newprefix)
+                valuedict.update(returndict)
+
+            return valuedict
+
+        else:
+            keyname = prefix
+            valuedict[keyname] = data
+
+            return valuedict
